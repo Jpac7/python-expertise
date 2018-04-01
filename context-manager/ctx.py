89b8e66941d67ec8701139b079a7cfafa21e929f
Jpac <jose.costa@tradingeconomics.com>
2018-04-02 00:49:10 +0100

    Context managers in action

---
+++ context-manager/ctx.py
@@ -0,0 +1,4 @@
+# CONTEXT MANAGERS - WRAPPERS FOR OPERATIONS W/ SETUP AND FINAL ACTIONS INCLUDED
+# The idea is there is some initial (or setup) action and some final (or teardown) action
+# that you want to mash them together
+# Examples - open and close files, connect and disconnect from databases...
